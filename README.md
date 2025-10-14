9dcfb28b2a4dab801f0b1de702e8212acdbbe525
Sam B <107863539+Sam-Bieberich@users.noreply.github.com>
2025-10-14 09:29:37 -0500

    mig partitioning in to 7 bash file added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Partitioning setup files
+# Partitioning setup files
 
+## File Explanations
+
+* Run the mig_setup_7.sh bash file to create seven equal partitions of the GPU on a node. Requires sudo access. To test that sudo works, you can run sudo -v
---
+++ mig_setup_7.sh
@@ -0,0 +1,36 @@
+echo "Starting MIG setup (7 partitions)"
+
+#checking that MIG is enabled
+echo "MIG partitioning enabled check"
+
+sudo nvidia-smi -i 0 -mig 1
+
+echo "--------------------------"
+
+
+echo "Deleting any old instances"
+
+sudo nvidia-smi mig -dci -i 0   # Delete all compute instances
+sudo nvidia-smi mig -dgi -i 0   # Delete all GPU instances
+
+echo "MIG Profiles"
+
+nvidia-smi mig -lgip
+
+echo "--------------------------"
+
+
+echo "Creating 7 partitions"
+sudo nvidia-smi mig -cgi 19,19,19,19,19,19,19 -C
+
+nvidia-smi -L
+
+echo "--------------------------"
+
+# How to use this script:
+
+# Make the script executable (if not already)
+# chmod +x mig_setup_7.sh
+
+# Run the script
+# ./mig_setup_7.sh
