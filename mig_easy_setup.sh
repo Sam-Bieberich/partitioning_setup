4c3b2362a2fc4d37d364be67dee7973a2716fa21
Sam B <107863539+Sam-Bieberich@users.noreply.github.com>
2025-10-15 09:26:36 -0500

    robust mig addition

--- mig_easy_setup.sh
+++ mig_easy_setup.sh
@@ -21,7 +21,12 @@ echo "--------------------------"
 
 
 echo "Creating 7 partitions"
-sudo nvidia-smi mig -cgi 19,19,19,19,19,19,19 -C
+
+GI_ID=$(nvidia-smi mig -lgip | awk '/1g\./{print $2,$0}' | awk '$1 ~ /[0-9]+/ {print $1}' | head -n1)
+if [ -z "$GI_ID" ]; then echo "ERROR: Could not find 1g.* GI profile id"; exit 1; fi
+sudo nvidia-smi mig -cgi $GI_ID -C --count 7
+
+# sudo nvidia-smi mig -cgi 19,19,19,19,19,19,19 -C
 
 echo "Confirming partitions"
 
